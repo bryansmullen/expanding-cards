e452113d30764bf392a11f9e41673a077fea3c61
Bryan Mullen <bryan@bryanmullen.me>
2021-03-12 19:30:17 +0000

    Normalise CSS and Render App

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reset from 'react-style-reset/string';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+`;
 
 ReactDOM.render(
     <React.StrictMode>
+        <GlobalStyles />
         <App />
     </React.StrictMode>,
 
     document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
